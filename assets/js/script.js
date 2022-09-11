a44715739ba66ecdc1f82e61cf3f50f6862104b4
KeoCode <keocode101@gmail.com>
2022-09-11 21:04:38 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1,num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -57,7 +59,9 @@ let operator = document.getElementById("operator").innerText;
 
 if (operator === "+") {
     return [operand1 + operand2, "addition"];
-} else {
+} else if (operator === "x") {
+    return [operand1 * operand2, "multiply"];
+}else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}.Aborting!`;
 }
@@ -92,11 +96,14 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractionQuestion() {
+function displaySubtractQuestion() {
    
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
 
